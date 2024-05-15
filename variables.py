0b899a331b0e3a4d8ba855c1101fa942a443d30f
Guilherme <francaguilherme27@gmail.com>
2024-05-15 14:49:10 -0300

    feat: Variables in Python

---
+++ variables.py
@@ -0,0 +1,23 @@
+name = "Guilherme França"
+
+age = 23
+
+email = "francaguilherme27@gmail.com"
+
+print("Nome:", name + "\n" + "Idade:", age, "\n" + "E-mail:", email)
+
+name1, age1, email1 = "Evelin Mejia", 21, "eve123@gmail.com"
+
+print("\nNome:", name1 + "\n" + "Idade:", age1, "\n" + "E-mail:", email1 + "\n")
+
+a = 5
+b = 6
+c = a
+a = 1
+
+print(a)
+print(c)
+
+a = "text"
+
+print(a)
