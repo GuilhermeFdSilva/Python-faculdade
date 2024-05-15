72046bf8c07b2b62b229e6798ea57ce5860b9a09
Guilherme <francaguilherme27@gmail.com>
2024-05-15 14:49:38 -0300

    feat: Input function

---
+++ input.py
@@ -0,0 +1,11 @@
+name = input("Digite seu nome: ")
+
+age = int(input("Olá" + name + ", quantos anos você tem? "))
+
+weight = float(input("Qual seu peso? "))
+
+print("\n ---- Ficha ----")
+
+print("Nome:", name)
+print("Idade:", age)
+print("Peso:", weight, "\n")
