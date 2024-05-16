ccb262595c7764b2e8abd0cb3c6388d0732efaf6
Guilherme <francaguilherme27@gmail.com>
2024-05-16 13:47:06 -0300

    feat: Relational operators

---
+++ relational_operators.py
@@ -0,0 +1,19 @@
+a = 5
+b = 5
+if (a == b):
+    print("a é igual a b")
+    
+a = 5
+b = 2
+if (a != b):
+    print("a é diferente a b")
+    
+a = 5
+b = 2
+if (a > b):
+    print("a é maior a b")   
+    
+a = 1
+b = 2
+if (a < b):
+    print("a é menor a b")
