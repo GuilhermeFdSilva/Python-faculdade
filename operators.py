51e581ba86ad02dc5e6220420df25d02bcbb6ac3
Guilherme <francaguilherme27@gmail.com>
2024-05-16 13:46:28 -0300

    feat: Arithmetic operators

---
+++ operators.py
@@ -0,0 +1,29 @@
+a = 2
+b = 3
+
+resultado = a + b
+print(resultado)
+
+resultado = a - b
+print(resultado)
+
+resultado = a * b
+print(resultado)
+
+resultado = a / b
+print(resultado)
+
+resultado = a ** b
+print(resultado)
+
+resultado = a // b
+print(resultado)
+
+# Precedencia
+a = 20 
+b = 10 
+c = 15 
+d = 5 
+e = (a + b) * c / d 
+
+print(e)
