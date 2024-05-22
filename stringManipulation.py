1a4d654d4454c96e59788272e54bd6d744aca001
Guilherme <francaguilherme27@gmail.com>
2024-05-22 12:20:37 -0300

    feat: String manipulation

---
+++ stringManipulation.py
@@ -0,0 +1,20 @@
+myString = "Olá"
+print(myString)
+
+myString = 'Olá!'
+print(myString)
+
+myString = '''Olá!!!'''
+print(myString)
+
+# Textos de multipla linha
+myString = """Olá, bem vindo
+            ao mundo do Python"""
+print(myString)
+
+print(myString[0])
+
+if "Olá" in myString:
+    print("Sequência encontrada")
+else:
+    print("Sequência não encontrada")
